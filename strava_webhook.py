ef8f0d5bc40a8d7860e7f3040ac62960fc1b0602
Caleb Frost Sanderson <me@calebsanderson.com>
2025-06-14 03:49:30 -0400

    removing useless values

--- strava_webhook.py
+++ strava_webhook.py
@@ -235,10 +235,7 @@ def build_fulcrum_payload(activity, geojson):
         "4840": round_or_none(meters_to_feet(activity.get("total_elevation_gain"))),
         "d000": round_or_none(meters_to_feet(activity.get("elev_low"))),
         "6767": round_or_none(meters_to_feet(activity.get("elev_high"))),
-        "3350": celsius_to_fahrenheit(activity.get("average_temp")),
-        "8f50": None, #min temp
-        "5270": None, #max temp
-        "446e": None, #aerobic TE
+        "3350": celsius_to_fahrenheit(activity.get("average_temp"))
     }
     # Convert all non-None values to strings (Fulcrum expects string values)
     form_values = {k: str(v) for k, v in form_values.items() if v is not None}
